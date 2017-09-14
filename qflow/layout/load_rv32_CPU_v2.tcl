32896934076bc900c2c33d610dab155d339ff253
Mohamed Kassem <mkk@efabless.com>
2017-09-14 15:51:21 -0700

    added qflow implementation for xh035

---
+++ qflow/layout/load_rv32_CPU_v2.tcl
@@ -0,0 +1,4 @@
+lef read /ef/tech/XFAB/EFXH035B/libs.ref/lef/D_CELLS/xh035_D_CELLS.lef
+load rv32_CPU_v2
+select top cell
+expand
---
+++ qflow/qflow_vars.sh
@@ -0,0 +1,16 @@
+#!/bin/tcsh -f
+#-------------------------------------------
+# qflow variables for project /ef-design/projects/dwarfRV32/qflow
+#-------------------------------------------
+
+set projectpath=/ef-design/projects/dwarfRV32/qflow
+set techdir=/ef/apps/ocd/qflow/1.1.70/share/qflow/tech/EFXH035B
+set sourcedir=/ef-design/projects/dwarfRV32/qflow/source
+set synthdir=/ef-design/projects/dwarfRV32/qflow/synthesis
+set layoutdir=/ef-design/projects/dwarfRV32/qflow/layout
+set techname=EFXH035B
+set scriptdir=/ef/apps/ocd/qflow/1.1.70/share/qflow/scripts
+set bindir=/ef/apps/ocd/qflow/1.1.70/share/qflow/bin
+set logdir=/ef-design/projects/dwarfRV32/qflow/log
+#-------------------------------------------
+
